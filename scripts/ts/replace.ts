afb9d3fae40f926eb4f68a2cf3217a0d6baf21e4
Josh <43808099+josh-codes@users.noreply.github.com>
2021-08-27 18:03:39 +0800

    Changed replace script and added npm run i

--- scripts/ts/replace.ts
+++ scripts/ts/replace.ts
@@ -1,6 +1,8 @@
-import { readdirSync, readFileSync, writeFileSync, Dirent } from 'fs';
+import { renameSync, readdirSync, readFileSync, writeFileSync, Dirent } from 'fs';
 import { join as fspathjoin, resolve } from 'path';
 const pathloc = fspathjoin(resolve(), "/../app/pages");
+const oldpath = fspathjoin(resolve(), "/../app/_app");
+const newpath = fspathjoin(resolve(), "/../app/appdata");
 const regex = /(.*\.html)/;
 const fixfile = async (path:string, reqcount:number) => {
     let dotdotslash = "../";
@@ -8,10 +10,16 @@ const fixfile = async (path:string, reqcount:number) => {
         dotdotslash+="../"
     }
     let data = await readFileSync(path, "utf8")
-    let result = data.split("/_app").join(dotdotslash+"_app");
+    let result = data.split("/_app").join(dotdotslash+"appdata");
     result = result.split("from \"").join("from \"./");
     result = result.split("import(\"").join("import(\"./");
+    const js = result.split("<script type=\"module\">")[1].split("</script>")[0]
+    result = result.split("<script type=\"module\">")[0]+`<link rel=\"stylesheet\" href=\"/fonts/font.css\"/><link rel=\"icon\" href=\"/icons/full.png\" /><script defer type=\"module\" src=\"init.mjs\"></script>`+result.split("</script>")[1]
     await writeFileSync(path, result);
+    let initjspath:string[]|string = path.split("/");
+    initjspath.pop();
+    initjspath = fspathjoin(initjspath.join("/"), "/init.mjs");
+    await writeFileSync(initjspath, js);
 }
 const recursive = async (path:string, reqcount:number) => {
     const folder:Dirent[] = await readdirSync(path, { withFileTypes: true });
@@ -23,4 +31,5 @@ const recursive = async (path:string, reqcount:number) => {
         }
     })
 }
+renameSync(oldpath, newpath)
 recursive(pathloc, 0)
